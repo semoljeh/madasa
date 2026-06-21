dbd459b6f3680d25543625e02811a394ae273cd3
semoljeh <aromtemplate@gmail.com>
2026-06-21 21:03:22 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'madasa-pwa-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './rapor_tpq.html',
+  './rapor_ibtidaiyah.html',
+  './rapor_sanawiyah.html',
+  './asset/logo.png',
+  './asset/logo-192.png',
+  './asset/logo-512.png'
+];
+
+// Install Service Worker dan simpan file ke cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Ambil file dari cache jika tidak ada internet / agar lebih cepat
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; 
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Hapus cache lama jika ada update
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
