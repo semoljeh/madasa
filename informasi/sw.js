4947c6a7af6e1d3cc2126fe919a719c20cc1b8b9
semoljeh <aromtemplate@gmail.com>
2026-07-30 18:55:58 +0700

    Delete informasi directory

--- informasi/sw.js
+++
@@ -1,8 +0,0 @@
-// File: informasi/sw.js
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-    // Biarkan kosong untuk sekadar memicu syarat PWA
-});
