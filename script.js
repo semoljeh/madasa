54e6fe7110e2584459d718759606eeb084206c22
semoljeh <aromtemplate@gmail.com>
2026-09-09 18:19:48 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3782,7 +3782,7 @@ const romawi = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
                 ${diktumHtml}
             </table>
 
-            <div class="ttd-area">
+           <div class="ttd-area">
                 <p>Ditetapkan di : Bangkalan</p>
                 <p>Pada tanggal  : ${tglMasehi}</p>
                 <p style="font-weight: bold; margin-top: 10px;">Kepala Madrasah Darussalam,</p>
@@ -3792,6 +3792,9 @@ const romawi = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
                 </div>
                 <p class="nama">KH. UMAR FARUQ</p>
             </div>
+            
+            <!-- TAMBAHKAN KODE INI UNTUK MENCEGAH OVERLAP KE HALAMAN 2 -->
+            <div style="clear: both;"></div>
 
             <!-- HALAMAN 2: LAMPIRAN I (BINTANG KELAS) -->
             <div class="page-break"></div>
